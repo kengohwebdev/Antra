e298eef9dd1a554531699ad956934407b1096c9a
megahigh5 <102623149+megahigh5@users.noreply.github.com>
2022-06-02 15:41:09 -0500

    Add files via upload

---
+++ Assignment - JS/antra.js
@@ -0,0 +1,80 @@
+
+/*// Problem 1//*/
+// let salaries = 
+// {
+//     John :100,
+//     Ann: 160,
+//     Pete:130
+// }
+// let sum =0;
+// for (let key in salaries)
+// {    
+//     sum+= salaries[key]; 
+// }
+// console.log(sum);
+ 
+
+/*// Problem 2//*/
+// let menu = 
+// {
+//     width : 200,
+//     height: 300,
+//     title: "My menu"
+// }
+
+// function multiplyNumeric(obj) {
+//     for (let key in obj) {
+//       if (typeof obj[key] == 'number') {
+//         obj[key] *= 2;
+//       }
+//     }
+//   }
+//   multiplyNumeric(menu);
+//   console.log(menu);
+
+
+
+/*// Problem 3//*/
+// let str = "ken_@hotmail.com";
+// function checkEmailId(str){
+//     var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+//     return re.test(str);
+// }
+
+
+
+/*// Problem 4//*/
+// function truncate(str, maxlength) {
+//     return (str.length > maxlength) ?
+//       str.slice(0, maxlength - 1) + '…' : str;
+//   }
+
+
+
+/*// Problem 5//*/
+// let arr = ["James", "Brennie"];
+// arr.push("Robert");
+
+
+//     const mid = Math.floor(arr.length / 2);
+//     console.log(mid);
+
+
+
+// for(let i = 0; i< arr.length; i++)
+// {
+    
+//     if(i==mid)
+//     {
+//         arr[i] = "Calvin";
+//     }
+// }
+
+// arr.shift();
+// arr.unshift("Rose", "Regal");
+
+// for(let item in arr){
+//     console.log(arr[item])
+// }
+
+
